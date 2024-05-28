7d3d1413c9c238d2b9c2eef45d7a8b43c74d10aa
gabriel dias <146786791+super-gabriel-dias@users.noreply.github.com>
2024-05-28 19:14:27 -0300

    Add files via upload

---
+++ Desafio01.java
@@ -0,0 +1,80 @@
+package aula03_desafio;
+
+public class Desafio01 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Alô mundo\n");
+
+	}
+
+}
+
+
+/*
+
+1. Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+
+
+2. Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+
+
+3. Faça um Programa que peça dois números e imprima a soma.
+
+
+4. Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+
+5. Faça um Programa que converta metros para centímetros.
+
+
+6. Faça um Programa que pergunte quanto você ganha por hora e o número de horas
+
+trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
+
+ 
+
+7. Faça um Programa que peça a temperatura em graus Farenheit, transforme e mostre a
+
+temperatura em graus Celsius.
+
+C = (5 * (F-32) / 9) / C = (F – 32) / 1,8
+
+
+8.Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre:
+
+a. o produto do dobro do primeiro com metade do segundo .
+
+b. a soma do triplo do primeiro com o terceiro.
+
+c. o terceiro elevado ao cubo.
+
+
+9.Tendo como dado de entrada a altura, construa um algoritmo que calcule seu peso ideal,
+
+utilizando as seguintes fórmulas:
+
+Para homens: (72.7*h) - 58
+
+Para mulheres: (62.1*h) - 44.7 (h = altura)
+
+
+10. Faça um Programa que pergunte quanto você ganha por hora e o número de horas
+
+trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês, sabendo-se
+
+que são descontados 11% para o Imposto de Renda, 8% para o INSS e 5% para o
+
+sindicato, faça um programa que nos dê:
+
+a. salário bruto.
+
+b. quanto pagou ao INSS.
+
+c. quanto pagou ao sindicato.
+
+d. o salário líquido.
+
+Obs.: Salário Bruto - Descontos = Salário Líquido.
+
+*/
