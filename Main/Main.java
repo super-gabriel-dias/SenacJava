6bb4dfc508fe18328f2b46618acbf090c70859f6
gabriel dias <146786791+super-gabriel-dias@users.noreply.github.com>
2024-05-28 20:11:41 -0300

    Create Main.java

---
+++ Main/Main.java
@@ -0,0 +1,35 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+    // System.out.println("Hello world!");
+
+    // Variaveis do tipo booleano
+    boolean media = true;
+    boolean frequencia = true;
+    boolean aprovado = media && frequencia;
+    boolean cafe = false;
+    boolean agua = true;
+    boolean clienteServido = cafe || agua;
+
+    // Variaveis do tipo inteiro
+    int idade = 20;
+
+    // Variavel do tipo real
+    double salario = 1000.00;
+    float temperatura = 25.8f;
+
+    // Variaveis do tipo texto
+    String nome = "Kalil";
+    String cpf = "12345678901";
+    String aniversario = "01/01/1990";
+
+
+    // Método de saida
+     System.out.println("Cliente servido? " + clienteServido+"\n");
+     System.out.println("Cliente servido? " + clienteServido);
+
+  }
+}
